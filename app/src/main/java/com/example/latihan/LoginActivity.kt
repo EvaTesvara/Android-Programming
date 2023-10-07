0500a295d0d2933d118f43f4ff5eff14be2d1a8c
Eva Tesvara <eva.tesvara@gmail.com>
2023-10-07 15:40:06 +0700

    intent data pass

--- app/src/main/java/com/example/latihan/LoginActivity.kt
+++ app/src/main/java/com/example/latihan/LoginActivity.kt
@@ -5,11 +5,25 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
 
 class LoginActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var etUsername:EditText
+    private lateinit var etPassword:EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        etUsername = findViewById(R.id.edt_username)
+        etPassword = findViewById(R.id.edt_password)
+
+        val isii = intent.extras
+        if (isii!=null){
+            etUsername.setText(isii.getString("username"))
+            etPassword.setText(isii.getString("password"))
+        }
 
         val btnLogin: Button = findViewById(R.id.btn_login)
         btnLogin.setOnClickListener(this)
